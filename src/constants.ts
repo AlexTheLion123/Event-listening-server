8d836d70211824c580b6da232130ca44b6b4a1a2
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2024-04-30 01:15:56 +0200

    update deployment address

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const CONTRACTADDRESS = '0x5981e73f2D7E934A2242123B89faCa939BB5ccAf';
+export const CONTRACTADDRESS = '0x7EACc490BCFC3E13D746B3575c07068Ea0809F49';
 export const CONTRACTABI = [
     "event BetCreated(uint256 indexed betId, address indexed player1, uint256 amount, uint256 targetPrice, bool isHigherChosen, uint256 targetTimestamp)",
     "function createBet(uint256 _targetPrice, bool _isHigherChosen, uint256 _targetTimestamp) external payable",
